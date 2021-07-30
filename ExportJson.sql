98905be534f0cb65b3cadee8a08618ef8e226162
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-07-30 13:34:28 +0530

    Add files via upload

---
+++ ExportJson.sql
@@ -0,0 +1,17 @@
+Drop table Issues;
+Create table Issues( Issue_title NCHAR(50),Issue_state NCHAR(50), Issue_Number NCHAR(50),Issue_Opened_By NCHAR(50),Issued_Date NCHAR(50) )
+SELECT Issue_title,Issue_state, Issue_Number,Issue_Opened_By,Issued_Date FROM OPENROWSET(
+    BULK  'Blob/commit.json',
+    DATA_SOURCE = 'MyAzureBlobStorage25',
+     SINGLE_CLOB
+     ) AS DataFile
+ CROSS APPLY OPENJSON(BulkColumn)
+ WITH (Issue_title NCHAR(50),Issue_state NCHAR(50), Issue_Number NCHAR(50),Issue_Opened_By NCHAR(50),Issued_Date NCHAR(50)) as book
+INSERT INTO books with (TABLOCK) (Issue_title,Issue_state,Issue_Number,Issue_Opened_By,Issued_Date)
+ SELECT Issue_title,Issue_state,Issue_Number,Issue_Opened_By,Issued_Date FROM OPENROWSET(
+    BULK  'Blob/commit.json',
+    DATA_SOURCE = 'MyAzureBlobStorage25',
+     SINGLE_CLOB
+     ) AS DataFile
+ CROSS APPLY OPENJSON(BulkColumn)
+ WITH (Issue_title NCHAR(50),Issue_state NCHAR(50), Issue_Number NCHAR(50),Issue_Opened_By NCHAR(50),Issued_Date NCHAR(50)) as Issue
