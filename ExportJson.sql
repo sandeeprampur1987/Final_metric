3420e3428569b6a4c559cd187694170205b8bee1
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-08-02 15:23:16 +0530

    Update ExportJson.sql

--- ExportJson.sql
+++ ExportJson.sql
@@ -1,7 +1,7 @@
 Drop table Issues;
 Create table Issues( Issue_title NCHAR(50),Issue_state NCHAR(50), Issue_Number NCHAR(50),Issue_Opened_By NCHAR(50),Issued_Date NCHAR(50) )
 SELECT Issue_title,Issue_state, Issue_Number,Issue_Opened_By,Issued_Date FROM OPENROWSET(
-    BULK  'Blob/outputFinal2.json',
+    BULK  'Blob/Issues_Final.json',
     DATA_SOURCE = 'MyAzureBlobStorage25',
      SINGLE_CLOB
      ) AS DataFile
@@ -9,7 +9,7 @@ SELECT Issue_title,Issue_state, Issue_Number,Issue_Opened_By,Issued_Date FROM OP
  WITH (Issue_title NCHAR(50),Issue_state NCHAR(50), Issue_Number NCHAR(50),Issue_Opened_By NCHAR(50),Issued_Date NCHAR(50)) as book
 INSERT INTO Issues with (TABLOCK) (Issue_title,Issue_state,Issue_Number,Issue_Opened_By,Issued_Date)
  SELECT Issue_title,Issue_state,Issue_Number,Issue_Opened_By,Issued_Date FROM OPENROWSET(
-    BULK  'Blob/outputFinal2.json',
+    BULK  'Blob/Issues_Final.json',
     DATA_SOURCE = 'MyAzureBlobStorage25',
      SINGLE_CLOB
      ) AS DataFile
