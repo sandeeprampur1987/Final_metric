c0bd6996f9b45039b1c85c9abd07155b0910b9a1
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-08-02 15:32:22 +0530

    Update ExportJson.sql

--- ExportJson.sql
+++ ExportJson.sql
@@ -2,7 +2,7 @@ Drop table Issues;
 Create table Issues( Issue_title NCHAR(50),Issue_state NCHAR(50), Issue_Number NCHAR(50),Issue_Opened_By NCHAR(50),Issued_Date NCHAR(50) )
 SELECT Issue_title,Issue_state, Issue_Number,Issue_Opened_By,Issued_Date FROM OPENROWSET(
     BULK  'Blob/Issues_Final.json',
-    DATA_SOURCE = 'MyAzureBlobStorage25',
+    DATA_SOURCE = 'MyAzureBlobStorage26',
      SINGLE_CLOB
      ) AS DataFile
  CROSS APPLY OPENJSON(BulkColumn)
@@ -10,7 +10,7 @@ SELECT Issue_title,Issue_state, Issue_Number,Issue_Opened_By,Issued_Date FROM OP
 INSERT INTO Issues with (TABLOCK) (Issue_title,Issue_state,Issue_Number,Issue_Opened_By,Issued_Date)
  SELECT Issue_title,Issue_state,Issue_Number,Issue_Opened_By,Issued_Date FROM OPENROWSET(
     BULK  'Blob/Issues_Final.json',
-    DATA_SOURCE = 'MyAzureBlobStorage25',
+    DATA_SOURCE = 'MyAzureBlobStorage26',
      SINGLE_CLOB
      ) AS DataFile
  CROSS APPLY OPENJSON(BulkColumn)
